2449ce16afde8ccaa8819cebdd97fdae10171819
Parmeshwar Nagre <parmeshwar.nagre@thinkitive.com>
2023-02-04 14:33:31 +0530

    db added

---
+++ MailerStackWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using MailerStackWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MailerStackWeb.Data
+{
+    
+        public class ApplicationDbContext : DbContext
+        {
+            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+            {
+
+            }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Recipient>()
+                .HasNoKey();
+        }
+        public DbSet<User> User { get; set; }
+        public DbSet<AddedRecipients> AddedRecipients { get; set; }
+        public DbSet<AddRecipientRequest> AddRecipientRequest { get; set; }
+
+        public DbSet<Campaign> Campaign { get; set; }
+        public DbSet<CampaignExportRequest> CampaignExportRequest { get; set; }
+        public DbSet<CampaignExport> CampaignExport { get; set; }
+        public DbSet<Click> Click { get; set; }
+        public DbSet<CreatedLeads> CreatedLeads { get; set; }
+        public DbSet<EmailAddress> EmailAddress { get; set; }
+        public DbSet<Error> Error { get; set; }
+
+        public DbSet<Lead> Lead { get; set; }
+        public DbSet<LeadStatus> LeadStatus { get; set; }
+        public DbSet<Message> Message { get; set; }
+        public DbSet<Open> Open { get; set; }
+        public DbSet<Recipient> Recipient { get; set; }
+        public DbSet<Reply> Reply { get; set; }
+        public DbSet<Sender> Sender { get; set; }
+        public DbSet<SentMessage> SentMessage { get; set; }
+        public DbSet<Team> Team { get; set; }
+
+       
+
+    }
+}
+
---
+++ MailerStackWeb/Models/Campaign.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+
+namespace MailerStackWeb.Models
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class CampaignMessage
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("subject")]
+        public string Subject { get; set; }
+
+        [JsonProperty("replyToID")]
+        public int? ReplyToID { get; set; }
+
+        [JsonProperty("isPaused")]
+        public bool IsPaused { get; set; }
+    }
+
+    public class Campaign
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+
+        [JsonProperty("isArchived")]
+        public bool IsArchived { get; set; }
+
+        [JsonProperty("isPaused")]
+        public bool IsPaused { get; set; }
+
+        [JsonProperty("messages")]
+        public List<CampaignMessage> Messages { get; set; }
+
+        [JsonProperty("sender")]
+        public CampaignSender Sender { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        [JsonProperty("createdAt")]
+        public DateTime createdAt { get; set; }
+
+        [JsonProperty("updatedAt")]
+        public DateTime updatedAt { get; set; }
+    }
+
+    public class CampaignSender
+    {
+        [JsonProperty("object")]
+        public string Object { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("emailAddress")]
+        public string EmailAddress { get; set; }
+
+        [JsonProperty("fromName")]
+        public string FromName { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime Created { get; set; }
+    }
+
+
+}
