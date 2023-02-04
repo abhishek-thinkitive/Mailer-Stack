a22e40aa119764f1bfa9cfb2bdb5071e1deac084
linesh-thinkitive <linesh.lanjewar@thinkitive.com>
2023-02-04 15:07:43 +0530

    created routes

---
+++ MailerStackWeb/Controllers/LeadsController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailerStackWeb.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class LeadsController : ControllerBase
+    {
+        public LeadsController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult ListAll()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Route("{leadId}")]
+        public ActionResult GetById(int leadId) {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("ignored")]
+        public ActionResult Ignored()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("reopen")]
+        public ActionResult Reopen()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ MailerStackWeb/Controllers/TeamsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MailerStackWeb.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/v1")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        public TeamsController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("{teamId}")]
+        public ActionResult GetAllMembersOfTeam(int teamId)
+        {
+            return Ok();
+        }
+    }
+}
